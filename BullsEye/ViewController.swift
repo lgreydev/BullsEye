a7fb896fff6a8d1ad6ca9f472fbf36c789b42db8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-30 19:55:44 +0300

    Add stores of the slider value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    /// Stores of the slider value
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,6 +34,8 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    /// Slider in which you can select a value in a specified range.
+    /// - Parameter slider: UISlider
     @IBAction func sliderMoved(_ slider: UISlider) {
         print(#line, "The value of the slider is now: \(slider.value)")
     }
