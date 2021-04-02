baf411a130d8fd4077fa92959399163fd3ea02d8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-04-02 23:07:08 +0300

    Add label for score

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,11 +15,17 @@ class ViewController: UIViewController {
     /// The random number that user will guess
     var targetValue = 0
     
+    /// Variable saves a common account
+    var score = 0
+    
     /// Label for the number to be guessed
     @IBOutlet var targetLabel: UILabel!
     
     /// Label for the slider
     @IBOutlet var slider: UISlider!
+    
+    /// Label for the score
+    @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +38,7 @@ class ViewController: UIViewController {
         currentValue = 50
         slider.value = Float(currentValue)
         updateLabels()
+        scoreLabel.text = String(score)
     }
     
     /// Method that updates target value
@@ -45,10 +52,19 @@ class ViewController: UIViewController {
         /// The difference between the slider and random value
         let difference = abs(currentValue - targetValue)
         
+        /// The number of points that receive the user.
+        let points = 100 - difference
+        
+        score += points
+        
         /// Message for alert 'showAlert'
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "You scored \(points) points"
+        
+        /*
+            "The value of the slider is: \(currentValue)"
             + "\nThe target value is: \(targetValue)"
             + "\nThe difference is: \(difference)"
+        */
         
         let alert = UIAlertController(
             title: "Hello, Wolrd!",
